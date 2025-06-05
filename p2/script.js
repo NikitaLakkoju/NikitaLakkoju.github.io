36e90ffa06cc3973342ffb932958d97e43da1188
nikita.lakkoju@gmail.com <nikita.lakkoju@gmail.com>
2025-06-05 19:53:00 +0530

    update

--- p2/script.js
+++ p2/script.js
@@ -2,8 +2,8 @@ const showLogin = () => {
     let str=`
     <div class='App-Container'>
     <h1>Login Form</h1>
-    <p><input type="text" id="txtEmail"></p>
-    <p><input type="password" id="txtPass"></p>
+    <p><input type="text" id="txtEmail" placeholder="Enter your Email"></p>
+    <p><input type="password" id="txtPass" placeholder="Enter your Password"></p>
     <p><button onclick='showWelcome()'> LOGIN </button></p>
     <p><button onclick='showRegister()'>Create Account</button></p>
     </div>
@@ -14,9 +14,9 @@ const showLogin = () => {
 const showRegister = () => {
       let str=`
     <h1>Register Form</h1>
-    <p><input type="text" id="txtName"></p>
-     <p><input type="text" id="txtEmail"></p>
-    <p><input type="password" id="txtPass"></p>
+    <p><input type="text" id="txtName" placeholder="Enter your Name"></p>
+     <p><input type="text" id="txtEmail" placeholder="Enter your Email"></p>
+    <p><input type="password" id="txtPass" placeholder="Enter your Password"></p>
     <p><button onclick='showLogin()'> Already have an account </button></p>
     <button>Register</button>
     `
